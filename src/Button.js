c51b64c2414ce14e3d3f8c3ea1e3a898ac836bdd
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-07-18 00:26:32 +0300

    install tailwindcss

--- src/Button.js
+++ src/Button.js
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 function Button({
   children,
   primary,
@@ -8,7 +10,11 @@ function Button({
   outline,
   rounded,
 }) {
-  return <button>{children}</button>;
+  return (
+    <button className="px-3 py-1.5 border border-blue-600 bg-blue-500 text-white">
+      {children}
+    </button>
+  );
 }
 
 Button.PropTypes = {
@@ -20,6 +26,8 @@ Button.PropTypes = {
     danger,
     outline,
     rounded,
+    onClick,
+    ...rest
   }) => {
     const count =
       Number(!!primary) +
