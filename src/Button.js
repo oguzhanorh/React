08a5345dd98621151f4d612382ad217c47b6c02c
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-07-18 00:15:35 +0300

    Create Custom Button

--- src/Button.js
+++ src/Button.js
@@ -1,5 +1,39 @@
-function Button({ children }) {
+function Button({
+  children,
+  primary,
+  secondary,
+  success,
+  warning,
+  danger,
+  outline,
+  rounded,
+}) {
   return <button>{children}</button>;
 }
+
+Button.PropTypes = {
+  checkVariationValue: ({
+    primary,
+    secondary,
+    success,
+    warning,
+    danger,
+    outline,
+    rounded,
+  }) => {
+    const count =
+      Number(!!primary) +
+      Number(!!secondary) +
+      Number(!!warning) +
+      Number(!!success) +
+      Number(!!danger) +
+      Number(!!outline) +
+      Number(!!rounded);
+
+    if (count > 1) {
+      return new Error('Only one of primary,secondary,..etc');
+    }
+  },
+};
 
 export default Button;
