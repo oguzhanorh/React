e0d27714406d7638888b0613c6ee7a9f6333d0d3
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-07-18 00:00:22 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Button from './Button';
+
+function App() {
+  return (
+    <>
+      <div>
+        <Button>Click Me</Button>
+      </div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+      <div>
+        <Button />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
