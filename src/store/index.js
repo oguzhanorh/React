c398e94491155d24d367be4b2f459705bd60b264
oguzhan.orh1907@gmail.com <oguzhan.orh1907@gmail.com>
2023-07-23 23:47:37 +0300

    create store and slice

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ const songsSlice = createSlice({
       state.push(action.payload);
     },
     removeSong(state, action) {
-      //
+      state.pop(action.payload);
     },
   },
 });
@@ -19,4 +19,19 @@ const store = configureStore({
   },
 });
 
-console.log(store);
+console.log(songsSlice.actions.addSong('some song')); //slice + actions + type
+
+const startingState = store.getState();
+console.log(JSON.stringify(startingState));
+
+store.dispatch(songsSlice.actions.addSong('Some Song!'));
+
+const finalState = store.getState();
+console.log(JSON.stringify(finalState));
+
+// store.dispatch({
+//   type: 'song/removeSong',
+// });
+
+// const removeState = store.getState();
+// console.log(JSON.stringify(removeState));
